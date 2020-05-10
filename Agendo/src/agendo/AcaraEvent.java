5e516783470dd5aa7be26c0f74be2868ad4bd8ae
benediktusaji <44857388+benediktusaji@users.noreply.github.com>
2020-05-10 20:39:42 +0700

    Method Class Event
    Buat method class event

---
+++ Agendo/src/agendo/AcaraEvent.java
@@ -0,0 +1,12 @@
+package agendo;
+
+import java.util.Date;
+
+public class AcaraEvent extends Event {
+
+	public AcaraEvent(String judul, Date tanggal, String deskripsi, String kategori) {
+		super(judul, tanggal, deskripsi, kategori);
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- Agendo/src/agendo/Event.java
+++ Agendo/src/agendo/Event.java
@@ -1,5 +1,48 @@
 package agendo;
 
+import java.util.Date;
+
 public class Event {
+	private String judul;
+	private Date tanggal;
+	private String deskripsi;
+	private String kategori;  //bisa dikembangin jadi class sendiri
+	
+	public Event(String judul, Date tanggal, String deskripsi, String kategori) {
+		this.judul = judul;
+		this.tanggal = tanggal;
+		this.deskripsi = deskripsi;
+		this.kategori = kategori;
+	}
+	
+	public void editEvent(String indikator, String newData) {
+		if(indikator.equals("judul")) {
+			this.judul = newData;
+		}else if(indikator.equals("deskripsi")) {
+			this.deskripsi = newData;
+		}else {
+			this.kategori = newData;
+		}
+	}
+	
+	public void editEvent(Date newData) {
+		this.tanggal = newData;
+	}
+
+	public String getJudul() {
+		return judul;
+	}
+
+	public Date getTanggal() {
+		return tanggal;
+	}
+
+	public String getDeskripsi() {
+		return deskripsi;
+	}
+
+	public String getKategori() {
+		return kategori;
+	}
 	
 }
